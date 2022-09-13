3ef83505e793c35b0d4d4899f3185bfcba289515
Ruslan Simeonov <ruslansimenov@gmail.com>
2022-09-13 15:15:36 +0200

    Added different styles to forms

--- seeds/index.js
+++ seeds/index.js
@@ -23,7 +23,7 @@ const seedDB = async () => {
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://picsum.photos/500/400',
+            image: 'https://picsum.photos/400/300',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
             price : price
         })
