1c1275e83f200e914610709c58f055cc28dee5ee
Ruslan Simeonov <ruslansimenov@gmail.com>
2022-09-14 13:01:09 +0200

    Error Handling

--- app.js
+++ app.js
@@ -58,11 +58,16 @@ app.get('/campgrounds/:id/edit', async(req, res) => {
     res.render('campgrounds/edit', {campground})
 })
 
-app.put('/campgrounds/:id', async(req, res) => {
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
-    res.redirect(`/campgrounds/${campground._id}`)
-})
+app.put('/campgrounds/:id', async(req, res, next) => {
+    try{
+        const {id} = req.params;
+        const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+        res.redirect(`/campgrounds/${campground._id}`)
+    
+    } catch (e) {
+        next(e)
+    }
+    })
 
 app.delete('/campgrounds/:id', async(req, res) => {
     const {id} = req.params;
