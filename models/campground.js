4d20259c810ec6ae8d4df7bb0ff4d9978b79ab83
Ruslan Simeonov <ruslansimenov@gmail.com>
2022-09-15 12:43:31 +0200

    Added review form, validation using JOI Schemas.

--- models/campground.js
+++ models/campground.js
@@ -6,7 +6,13 @@ const CampgroundsSchema = new Schema ({
     image: String,
     price: Number,
     description: String, 
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 })
 
 module.exports = mongoose.model('Campground', CampgroundsSchema)
