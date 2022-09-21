20551e9d29e7eb49b40a9c0f70e991144f71ab0d
Ruslan Simeonov <ruslansimenov@gmail.com>
2022-09-21 16:31:28 +0200

    Added Authorization for users and reviews

--- seeds/index.js
+++ seeds/index.js
@@ -5,7 +5,6 @@ const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true
 });
 
@@ -25,6 +24,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '5f5c330c2cd79d538f2c66d9', // hard coded User
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
