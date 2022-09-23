533d6d5db98b1dcf7961dd5ceedcd46289095809
Ruslan Simeonov <ruslansimenov@gmail.com>
2022-09-23 10:31:20 +0200

    Image upload, deleting images Adding thumbnails

--- models/campground.js
+++ models/campground.js
@@ -2,11 +2,20 @@ const mongoose = require('mongoose');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 
+//https://res.cloudinary.com/dweqzjnfv/image/upload/v1663917688/YelpCamp/hw71fyc0qpey39pp8wmr.jpg
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200')
+});
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {url: String, filename: String}
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
