42fb3f73fa885a9982ec6d3ca620a37d1c5e67f0
Ruslan Simeonov <ruslansimenov@gmail.com>
2022-09-28 14:31:42 +0200

    Prep for deployment

--- app.js
+++ app.js
@@ -16,12 +16,16 @@ const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize')
 const helmet = require('helmet')
 
-
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+const MongoDBStore = require('connect-mongo')(session);
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+//'mongodb://localhost:27017/yelp-camp'
+// mongoose.connect(dbUrl, {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -43,9 +47,24 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize());
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret'
+
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function(e){
+    console.log("SESSION STORE ERROR", e)
+})
+
+
+
 const sessionConfig = {
+    store,
     name: 'sessionNameNotDefault',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
